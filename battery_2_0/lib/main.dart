7d25800080f3c41f7003203205d8e9e2d560c3d4
SergeiSMV <s9051133401@yandex.ru>
2023-08-07 08:41:30 +0300

    testing 2

--- battery_2_0/lib/main.dart
+++ battery_2_0/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:intl/date_symbol_data_local.dart';
 
 import 'presentation/widgets/app.dart';
 
-// TESTING NUMBER 4
+// TESTING NUMBER 5
 
 void main() async {
   await GetStorage.init();
